5ab2fa506d782f9b9054860834257782c168f954
taptapking <lethanhbinhkts@gmail.com>
2024-08-06 14:40:01 +0700

    tweaked the flashing effect for the timer and life deduction

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -22,18 +22,26 @@ function ScoreBoard(props) {
             'border-radius': '20px',
             padding: '5px',
             border: '2px solid blue',
-            borderColor: ((parseInt(props.info.time) <= 1000) && (parseInt(props.info.time) % 2 === 0)) && "#DC143C",
+            borderColor: ((parseInt(props.info.time) <= 1000)) && "#DC143C",
             display: (props.info.gameStarted === 0) && "none"
         }}>
             <tr>
-                <td style={{ 'text-align': 'left', width: '33%', color: (!((parseInt(props.info.flash) <= 1000) && (parseInt(props.info.flash) % 2 === 0)) && 'black') || (((parseInt(props.info.flash) <= 1000) && (parseInt(props.info.flash) % 2 === 0)) && 'red') }}>
+                <td style={{
+                    'text-align': 'left', width: '33%',
+                    color: (!((parseInt(props.info.flash) <= 1000) && (parseInt(props.info.flash) % 2 === 0)) && 'black') || (((parseInt(props.info.flash) <= 1000) && (parseInt(props.info.flash) % 2 === 0)) && 'white'),
+                    backgroundColor: (!((parseInt(props.info.flash) <= 1000) && (parseInt(props.info.flash) % 2 === 0)) && 'white') || (((parseInt(props.info.flash) <= 1000) && (parseInt(props.info.flash) % 2 === 0)) && 'crimson')
+                }}>
                     <p id="p4">Lives left: {props.info.lives}</p>
                 </td>
-                <td style={{ 'text-align': 'center', width: '33%', display: (props.info.observeTime >= 0) && "none", color: ((parseInt(props.info.time) <= 1000) && (parseInt(props.info.time) % 2 === 0)) && "#DC143C" }}>
+                <td style={{
+                    'text-align': 'center', width: '33%', display: (props.info.observeTime >= 0) && "none",
+                    color: (!((parseInt(props.info.time) <= 1100) && (parseInt(props.info.time) % 100 <= 50)) && 'black') || (((parseInt(props.info.time) <= 1100) && (parseInt(props.info.time) % 100 <= 50)) && 'white'),
+                    backgroundColor: (!((parseInt(props.info.time) <= 1100) && (parseInt(props.info.time) % 100 <= 50)) && 'white') || (((parseInt(props.info.time) <= 1100) && (parseInt(props.info.time) % 100 <= 50)) && 'crimson'),
+                }}>
                     <p id="p3">Time: {msToTime(props.info.time)}</p>
                 </td>
                 <td style={{ 'text-align': 'center', width: '33%', display: (props.info.observeTime < 0) && "none" }}>
-                    <p id="p2">Time left to observe board: {msToTime(props.info.observeTime)}</p>
+                    <p id="p2">Observe time: {msToTime(props.info.observeTime)}</p>
                 </td>
                 <td style={{ 'text-align': 'right', width: '33%' }}>
                     <p id="p1">Current number: {props.info.current}</p>
